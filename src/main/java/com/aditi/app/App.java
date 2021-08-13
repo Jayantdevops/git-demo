a2be04b679affdb471545a95ff3b9d58eefad0ba
Jayantdevops <88762699+Jayantdevops@users.noreply.github.com>
2021-08-13 11:49:41 +0530

    chenges done to new branch

--- src/main/java/com/aditi/app/App.java
+++ src/main/java/com/aditi/app/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!learning java" );
+        System.out.println( "Hello World! branch" );
     }
 }
